8f02b34e9036298f5efc454aa0149a7d244e6032
sugenius <suuuuuuu1717@gmail.com>
2024-02-29 01:57:14 +0900

    API 확인

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,28 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name",name); //key, balue
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http 에서 헤더부와 바디부 중 바디부에 데이터를 직접 넣어주겠다라는 뜻
+    public String helloString(@RequestParam("name") String name) {
+        return "hello "+name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
