46a33686a490437e87e99324c95eccc8867fab7d
sugenius <suuuuuuu1717@gmail.com>
2024-03-07 18:13:20 +0900

    컴포넌트 스캔과 자동 의존관계 설정

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,28 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller // @Controller 사용하며 MemberController 객체를 생성하여 컨테이너에 등록하여 사용됨 ==> 스프링 컨테이너에서 스프링 빈이 관리된다.
+public class MemberController {
+
+    //private final MemberService memberService = new MemberService();
+    // new 키워드를 사용해도 되겠지, 하지만 스프링을 사용할때 스프링 컨테이너 에게 등록하고 관리되도록 사용하여야 한다.
+
+    private final MemberService memberService;
+
+    /* new 대신 생성자로 사용하고, @Autowired를 사용하도록 한다.
+    우선 스프링이 뜰 때, MemberController가 @Controller 에 의해 컨테이너에 등록된 상태에서
+    그때, 생성자를 호출하는데 생성자에 @Autowired가 선언되어 있다면
+    스프링 컨테이너에 있는 MemberService를 가져다 연결시켜주게 된다.
+
+    안될때(memberService 에서 빨간줄) , MemberService 를 가보면 순수 자바 클래스임을 알 수 있음
+    그땐 MemberService에서 @Service 어노테이션을 사용해야
+    @Service : 스프링이 스프링 컨테이너에 서비스를 등록해줌
+    * */
+    @Autowired
+    public MemberController(MemberService memberService){
+        this.memberService = memberService;
+    }
+}
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service //@Service : 스프링이 스프링 컨테이너에 서비스를 등록해줌
 public class MemberService {
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     private final MemberRepository memberRepository;
+
+    @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
