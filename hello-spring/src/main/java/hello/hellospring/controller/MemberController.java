e9b16c8359b2812df9f1ba7af321d795c4502b68
sugenius <suuuuuuu1717@gmail.com>
2024-03-11 17:04:15 +0900

    회원 웹 기능 - 조회

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // @Controller 사용하며 MemberController 객체를 생성하여 컨테이너에 등록하여 사용됨 ==> 스프링 컨테이너에서 스프링 빈이 관리된다.
 public class MemberController {
@@ -42,5 +45,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
